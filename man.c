d903680c082d7298a32467aa88e6a5ac20074678
v <wmswiderska@gmail.com>
2012-11-14 19:52:27 +0100

    runners are on the right places and they moving on the square - refactoring

--- man.c
+++ man.c
@@ -3,56 +3,7 @@
 #include <math.h>
 #include <GL/glut.h>
 #include <time.h>
-
-#define PI 			3.1415926535897
-#define MAX_NUMBER_OF_CHILDREN 	3
-
-#define MAX_NUMBER_OF_FRAMES 	40.0
-#define MAX_NUMBER_OF_BONES 	15
-#define MAX_NUMBER_OF_RUNERS	8
-
-#define STOP 		0
-#define START 		1
-
-#define FORWARD		0
-#define FORWARD_TURN	1
-#define BACKWARD	2
-#define BACKWARD_TURN	3
-#define PHASES		4
-
-
-#define HEAD 		0
-#define BACK 		1
-#define LEG_LEFT 	2
-#define ANKLE_LEFT	3
-#define FOOT_LEFT	4
-#define TOES_LEFT	5
-#define LEG_RIGHT	6
-#define ANKLE_RIGHT	7
-#define FOOT_RIGHT	8
-#define TOES_RIGHT	9
-#define ARM_LEFT	10
-#define FOREARM_LEFT	11
-#define ARM_RIGHT	12
-#define FOREARM_RIGHT	13
-
-typedef struct Bone_t {
-	double min_angle, max_angle;
-	double coord_x, coord_y, coord_z, angle, length;
-	int number_of_children;
-	int side;
-	struct Bone_t *child[MAX_NUMBER_OF_CHILDREN];
-	struct Bone_t *parent;
-} Bone;
-
-typedef struct Man_t {
-	Bone* tree_root;
-	double head_x, head_y, head_z, head_radius;
-	Bone* bones[MAX_NUMBER_OF_BONES];
-	int velocity;
-	int running_phase, number;
-	double phases[PHASES];
-} Man;
+#include "man.h"
 
 Man* runners[MAX_NUMBER_OF_RUNERS];
 
@@ -91,6 +42,10 @@ void init_runners() {
 		runners[i] -> velocity = 5.0;
 		runners[i] -> number = i;
 		runners[i] -> running_phase = FORWARD;
+		runners[i] -> phases[FORWARD] = 100;
+		runners[i] -> phases[BACKWARD] = -100;
+		runners[i] -> phases[FORWARD_TURN] = -100;
+		runners[i] -> phases[BACKWARD_TURN] = 100;
 
 	}
 
@@ -118,7 +73,7 @@ void running() {
 }
 
 void forward(Man* runner) {
-	if (runner -> head_x < 100) {
+	if (runner -> head_x < runner -> phases[FORWARD]) {
 		runner -> bones[HEAD] -> coord_x += runner -> velocity;
 		runner -> head_x += runner -> velocity;
 	} else {
@@ -128,7 +83,7 @@ void forward(Man* runner) {
 }
 
 void backward(Man* runner) {
-	if (runner -> head_x > -100) {
+	if (runner -> head_x > runner -> phases[BACKWARD]) {
 		runner -> bones[HEAD] -> coord_x -= runner -> velocity;
 		runner -> head_x -= runner -> velocity;
 	} else {
@@ -138,7 +93,7 @@ void backward(Man* runner) {
 }
 
 void forward_turn(Man* runner) {
-	if (runner -> head_z > -100) {
+	if (runner -> head_z > runner -> phases[FORWARD_TURN]) {
 		runner -> bones[HEAD] -> coord_z -= runner -> velocity;
 		runner -> head_z -= runner -> velocity;
 	} else {
@@ -148,7 +103,7 @@ void forward_turn(Man* runner) {
 }
 
 void backward_turn(Man* runner) {
-	if (runner -> head_z < 100) {
+	if (runner -> head_z < runner -> phases[BACKWARD_TURN]) {
 		runner -> bones[HEAD] -> coord_z += runner -> velocity;
 		runner -> head_z += runner -> velocity;
 	} else {
--- man.h
+++ man.h
@@ -1,13 +1,40 @@
 #ifndef MAN_H
 #define MAN_H
 
+#define PI 			3.1415926535897
+
 #define MAX_NUMBER_OF_CHILDREN 	3
+#define MAX_NUMBER_OF_FRAMES 	40.0
 #define MAX_NUMBER_OF_BONES 	15
 #define MAX_NUMBER_OF_RUNERS	8
 
+#define STOP 			0
+#define START 			1
+
+#define FORWARD			0
+#define FORWARD_TURN		1
+#define BACKWARD		2
+#define BACKWARD_TURN		3
+#define PHASES			4
+
+#define HEAD 			0
+#define BACK 			1
+#define LEG_LEFT 		2
+#define ANKLE_LEFT		3	
+#define FOOT_LEFT		4
+#define TOES_LEFT		5
+#define LEG_RIGHT		6
+#define ANKLE_RIGHT		7
+#define FOOT_RIGHT		8
+#define TOES_RIGHT		9
+#define ARM_LEFT		10
+#define FOREARM_LEFT		11
+#define ARM_RIGHT		12
+#define FOREARM_RIGHT		13
+
 typedef struct Bone_t {
-	double max_angle, min_angle;
-	double coord_x, coord_y, angle, length, depth;
+	double min_angle, max_angle;
+	double coord_x, coord_y, coord_z, angle, length;
 	int number_of_children;
 	int side;
 	struct Bone_t *child[MAX_NUMBER_OF_CHILDREN];
@@ -19,6 +46,8 @@ typedef struct Man_t {
 	double head_x, head_y, head_z, head_radius;
 	Bone* bones[MAX_NUMBER_OF_BONES];
 	int velocity;
+	int running_phase, number;
+	double phases[PHASES];
 } Man;
 
 Man* runners[MAX_NUMBER_OF_RUNERS];
@@ -29,4 +58,5 @@ void running();
 void draw_runner(Man* runner);
 
 void free_runners();
+
 #endif
